0590cea5457d4cf8024b2dc1737d2d53a91bc5a8
Gabriel Gomes <sougabriel815@gmail.com>
2024-10-07 21:55:17 -0300

    3

--- teste/exe_C.js
+++ teste/exe_C.js
@@ -6,21 +6,21 @@ frm.addEventListener ("submit", (e) =>{
     e.preventDefault()
     const valorTotal = Number(frm.inValue.value);
     //emitir "Valor Inválido"
-    if(valorTotal < 1|| valorTotal > 3){
+    if(valorTotal < 1){
         resposta2.innerText = "Valor Inválido";
     }
-    else if (valorTotal >= 1 || valorTotal < 1.74){
+    else if (valorTotal >= 1 && valorTotal < 1.75){
         resposta2.innerText = "Tempo:30 Minutos";
          resp = valorTotal - 1;
     }
-    else if(valorTotal >= 1.75 || valorTotal <= 2.9){
+    else if(valorTotal >= 1.75 && valorTotal <= 2.9){
         resposta2.innerText = "Tempo: 60 Minutos"
         resp = valorTotal - 1.75;
     }
-    else if(valorTotal =3){
+    else if(valorTotal >=3){
         resposta2.innerText ="Tempo: 120 Minutos"
         resp = valorTotal - 3;
     }
   
-    resposta.innerText = `O Troco é de: ${resp}`
+    resposta.innerText = `O Troco é de: ${resp.toFixed(2)}`
 })
