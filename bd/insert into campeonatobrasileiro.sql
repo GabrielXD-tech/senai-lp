46c7d33759dcb11d82c2cda1cc82e78863de0d52
GabrielXD-tech <sougabriel815@gmail.com>
2024-11-05 21:06:21 -0300

    Add files via upload

---
+++ bd/insert into campeonatobrasileiro.sql
@@ -0,0 +1,76 @@
+use campeonatobrasileiro;
+
+select * from estadio;
+select * from time;
+
+desc partida;
+
+insert into time(nome_completo, nome, sigla, cidade, estado, id_estadio)
+values('América Futebol Clube','América-MG','AME','Belo Horizonte','MG',1);
+
+insert into time(nome_completo, nome, sigla, cidade, estado, id_estadio)
+values('Club Atlhetico Paranaense','Atlhetico-PR','CAP','Curitiba','PR',1);
+
+insert into time(nome_completo, nome, sigla, cidade, estado, id_estadio)
+values('Club Atlhetico Mineiro','Atlhetico-MG','CAM','Belo Horizonte','MG',1);
+
+insert into time(nome_completo, nome, sigla, cidade, estado, id_estadio)
+values('Atlético Clube Goianiense','Atlhetico-GO','ACG','Goãnia','GO',1);
+
+insert into time(nome_completo, nome, sigla, cidade, estado, id_estadio)
+values('Atlético Clube Bahia','Bahia','BAH','Salvador','BA',1);
+
+insert into time(nome_completo, nome, sigla, cidade, estado, id_estadio)
+values('Botafogo de Futebol e Regatas','Botafogo','BOT','Rio de Janeiro','RJ',1);
+
+insert into time(nome_completo, nome, sigla, cidade, estado, id_estadio)
+values('Sport Club Corinthians Paulista','Corinthias','COR','São Paulo','SP',1);
+
+insert into time(nome_completo, nome, sigla, cidade, estado, id_estadio)
+values('Coritiba Foot Ball Club','Coritiba','CFC','Curitiba','PR',1);
+
+insert into time(nome_completo, nome, sigla, cidade, estado, id_estadio)
+values('Cruzeiro Esporte Clube','Cruzeiro','CRU','Belo Horizonte','MG',1);
+
+insert into time(nome_completo, nome, sigla, cidade, estado, id_estadio)
+values('Cuiabá Esporte Clube','Cuiabá','CUI','Cuiabá','MT',1);
+
+insert into time(nome_completo, nome, sigla, cidade, estado, id_estadio)
+values('Clube de Regatas do Flamengo','Flamengo','FLA','Rio de Janeiro','RJ',1);
+
+insert into time(nome_completo, nome, sigla, cidade, estado, id_estadio)
+values('Fluminense Football Club','Fluminense','FLU','Rio de Janeiro','RJ',1);
+
+insert into time(nome_completo, nome, sigla, cidade, estado, id_estadio)
+values('Fortaleza Esporte Clube','Fortaleza','FOR','Fortaleza','CE',1);
+
+insert into time(nome_completo, nome, sigla, cidade, estado, id_estadio)
+values('Goiás Esporte Clube','Goiás','GOI','Goiânia','GO',1);
+
+insert into time(nome_completo, nome, sigla, cidade, estado, id_estadio)
+values('Grêmio Foot-Ball Porto Alegrense','Grêmio','GRE','Porto Alegre','RS',1);
+
+insert into time(nome_completo, nome, sigla, cidade, estado, id_estadio)
+values('Sport Club Internacional','Internacional','INT','Porto Alegre','RS',1);
+
+insert into time(nome_completo, nome, sigla, cidade, estado, id_estadio)
+values('Palmeiras','Palmeiras','PAL','São Paulo','SP',1);
+
+insert into time(nome_completo, nome, sigla, cidade, estado, id_estadio)
+values('Red Bull Bragantino','Bragantino','RBB','Bragança','SP',1);
+
+insert into time(nome_completo, nome, sigla, cidade, estado, id_estadio)
+values('Santos Futebol Clube','Santos','SAN','Santos','SP',1);
+
+insert into time(nome_completo, nome, sigla, cidade, estado, id_estadio)
+values('São Paulo Futebol Clube','São Paulo','SAO','São Paulo','SP',1);
+
+
+
+
+
+
+
+
+
+
